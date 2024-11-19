25ff5365a27324e99fc0cc72489a004f0665e35c
selmasaltik <selmasaltiik@gmail.com>
2024-11-20 02:13:35 +0300

    Outputting a List of Meetups

--- 28-onwards-to-a-bigger-project-starter/pages/index.js
+++ 28-onwards-to-a-bigger-project-starter/pages/index.js
@@ -1,5 +1,24 @@
+import MeetupList from '@/components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 10, 54321 Some City',
+        description: 'This is a second meetup!'
+    }
+];
+
 function HomePage() {
-    return <h1>HomePage</h1>
+    return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage;
