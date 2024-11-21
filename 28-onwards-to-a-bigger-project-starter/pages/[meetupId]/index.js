327cfbf51b1e73602a157d1132ce80b4e4f3fb1e
selmasaltik <selmasaltiik@gmail.com>
2024-11-22 02:19:34 +0300

    Preparing Paths with "getStaticPaths" & Working With Fallback Pages

--- 28-onwards-to-a-bigger-project-starter/pages/[meetupId]/index.js
+++ 28-onwards-to-a-bigger-project-starter/pages/[meetupId]/index.js
@@ -11,10 +11,30 @@ function MeetupDetails(props) {
     );
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    };
+}
+
 export async function getStaticProps(context) {
     // fetch data for a single meetup
 
     const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
 
     return {
         props: {
